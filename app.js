b0a645d5e52948f6f89ebd106fb5ec59c9ad6cf1
AdelShbz <adelshahbaz5656@gmail.com>
2025-08-18 18:11:33 +0330

    update codes

--- app.js
+++ app.js
@@ -1,181 +1,152 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
+const bodyParser = require('body-parser')
+const { MongoClient, ObjectId } = require('mongodb');
 const bcrypt = require('bcryptjs');
+const { expressjwt: expressJWT } = require('express-jwt');
 const jwt = require('jsonwebtoken');
-const expressJWT = require('express-jwt');
+
 
 const app = express();
 const secretKey = 'ABSDFE!@#$%%$#@!1234'
+const jwtConfig = {
+    secret: secretKey,
+    algorithms: ['HS256'],
+    credentialsRequired: true
+};
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use('/note' , expressJWT({
-    secret: secretKey,
-    credentialsRequired: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/note' , expressJWT(jwtConfig))
 
 
-let theDb = undefined;
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri);
 
-app.post('/note', (req , res) => {
-    const note = { text } = req.body;
-    note.username = req.user.username;
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err , client) => {
-        if(err) {
-            res.send(err);
-            return;
-        }
-        theDb = client.db();
-        theDb.collection('note').insertOne(note, (err , result) => {
-            if(err){
-                res.send(err);
-                return;
-            }
-            res.send(note);
-        })
-    })
+app.post('/note', async (req, res) => {
+    try{
+        const note = {text} = req.body;
+        note.username = req.auth.username;
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('note');
+        
+        await collection.insertOne(note);
+        res.send(note)
+    }catch(err) {
+        res.send(err)
+    }
 })
 
-app.get('/note', (req, res) => {
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err , client) => {
-        if(err){
-            res.send(err);
-            return;
-        }
-        theDb = client.db();
-        theDb.collection('note').find({'username': req.user.username}).toArray((err , data) => {
-            if(err) res.status(500).send(err);
-            else res.send(data);
-        })
-    })
+app.get('/note', async (req, res) => {
+    try{
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('note');
+        const data = await collection.find({'username': req.auth.username}).toArray();
+        res.send(data);
+    }catch(err){
+        res.send(err)
+    }
 })
 
-app.put('/note', (req, res) => {
-    const note = { _id , text } = req.body;
-    const objId = new mongodb.ObjectID(note._id);
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err, client) => {
-        if (err){
-            res.send(err);
-            return;
-        }
-        theDb = client.db();
-        theDb.collection('note').find({'_id': objId}).toArray((err , data) => {
-            if (err) res.send(err);
-            else if (data[0].username !== req.user.username) {
-                res.send('you have not access to update this todo.');
-            } else {
-                theDb.collection('note').updateOne({_id: objId} , {$set : {text: note.text}} , (err , result) => {
-                    if (err) res.send(err);
-                    else res.send(result);
-                })
-            }
-        })
-    })
-})
-
-app.delete('/note/:id', (req, res) => {
-    const deleteId = req.params.id
-    const objId = new mongodb.ObjectID(deleteId);
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err , client) => {
-        if (err){
-            res.send(err);
+app.put('/note', async (req, res) => {
+    try{
+        const note = { _id , text } = req.body;
+        const objId = new ObjectId(note._id)
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('note');
+        const data = await collection.find({'_id': objId}).toArray();
+        if(data[0].username !== req.auth.username){
+            res.send('you have not access to update this note.');
         } else {
-            theDb = client.db();
-            theDb.collection('note').find({'_id': objId}).toArray((err , data) => {
-                if (err) res.send(err);
-                else {
-                    if(data[0].username !== req.user.username) {
-                        res.send('you have not access to delete this todo.');
-                    } else {
-                        theDb.collection('note').deleteOne({_id: objId} , (err , result) => {
-                            if (err) res.send(err);
-                            else res.send(result);
-                        })
-                    }
-                }
-            })
+            const result = await collection.updateOne({_id: objId}, {$set : {text: note.text}});
+            res.send(result)
         }
-    })
+        
+        
+    } catch(err) {
+        res.send(err)
+    }
 })
 
-
-
-app.post('/user/register', (req, res) => {
-    let credentials = { username , password } = req.body;
-    credentials.username = credentials.username.toLowerCase();
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err , client) => {
-        if(err) {
-            res.send(err);
-            return;
+app.delete('/note/:id', async (req, res) => {
+    try{
+        const deleteId = req.params.id;
+        const objId = new ObjectId(deleteId);
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('note');
+        const data = await collection.find({'_id': objId}).toArray()
+        if (data[0].username !== req.auth.username) {
+            res.send('you have not access to delete this note.');
+        } else {
+            const result = await collection.deleteOne({_id: objId})
+            res.send(result)
         }
-        theDb = client.db();
-        theDb.collection('user').find({'username' : credentials.username}).toArray((err , user) => {
-            if (err) res.send(err);
-            else if (user[0]) {
-                res.send({msg: 'this username has regestered already.', token: ''});
+    }catch(err){
+        res.send(err)
+    }
+})
+
+app.post('/user/register', async (req, res) => {
+    try{
+        let credentials = { username , password } = req.body;
+        credentials.username = credentials.username.toLowerCase();
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('user');
+        const user = await collection.find({'username': credentials.username}).toArray()
+        if(user[0]) {
+            res.send({msg: 'this username has regestered already.', token: ''});
+        } else {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(credentials.password , salt)
+            credentials.password = hashedPassword;
+            const token = jwt.sign({username: credentials.username} , secretKey)
+            credentials.tokens = credentials.tokens || [];
+            credentials.tokens.push(token)
+            await collection.insertOne( credentials );
+            res.send({msg: 'done', token: token})
+        }
+
+    }catch(err) {
+        res.send({msg: `${err}`, token: ''})
+    }
+})
+
+app.post('/user/login', async (req, res) => {
+    try{
+        const credentials = { username , password } = req.body;
+        await client.connect();
+        const database = client.db('OnlineNote');
+        const collection = database.collection('user');
+        const user = await collection.find({'username': credentials.username}).toArray()
+        if(!user[0]) {
+            res.send({msg:'this username not found.', token: ''});
+        } else {
+            const isMatch = await bcrypt.compare(credentials.password , user[0].password)
+            if(isMatch) {
+                const token = jwt.sign({username: credentials.username}, secretKey)
+                user[0].tokens = user[0].tokens || [];
+                user[0].tokens.push(token);
+                const objId = new ObjectId(user[0]._id);
+                delete user[0]._id;
+                await collection.updateOne({_id : objId} , {$set : user[0]})
+                res.send({msg:'done', token: token});
             } else {
-                bcrypt.genSalt(10, (err , salt) => {
-                    if (err) res.send({msg: `${err}`, token: ''});
-                    else {
-                        bcrypt.hash(credentials.password , salt , (err , hashedPassword) => {
-                            if (err) res.send({msg: `${err}`, token: ''});
-                            else {
-                                credentials.password = hashedPassword;
-                                const token = jwt.sign({username: credentials.username} , secretKey)
-                                credentials.tokens = credentials.tokens || [];
-                                credentials.tokens.push(token)
-                                theDb.collection('user').insertOne( credentials , ( err , result ) => {
-                                    if(err) res.send({msg: `${err}`, token: ''});
-                                    else {
-                                        res.send({msg: 'done', token: token})
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
+                res.send({msg:'the password is incorrect!', token: ''});
             }
-        })
-    })
-
-})
-
-app.post('/user/login', (req, res) => {
-    const credentials = { username , password } = req.body;
-    mongodb.connect('mongodb://localhost:27017/OnlineNote', { useNewUrlParser: true } , (err , client) => {
-        if(err) {
-            res.send(err);
-            return;
         }
-        theDb = client.db();
-        theDb.collection('user').find({'username': credentials.username}).toArray((err , user) => {
-            if (err) res.send(err);
-            else if (!user[0]) {
-                res.send({msg:'this username not found.', token: ''});
-            } else {
-                bcrypt.compare(credentials.password , user[0].password , (err , isMatch) => {
-                    if (err) res.send(err);
-                    else if (isMatch) {
-                        const token = jwt.sign({username: credentials.username}, secretKey)
-                        user[0].tokens = user[0].tokens || [];
-                        user[0].tokens.push(token);
-                        const objId = new mongodb.ObjectID(user[0]._id);
-                        delete user[0]._id;
-                        theDb.collection('user').updateOne({_id : objId} , {$set : user[0]} , (err , result) => {
-                            if (err) res.send({msg: `${err}`, token: ''});
-                            else {
-                                res.send({msg:'done', token: token});
-                            }
-                        })
-                    } else {
-                        res.send({msg:'the password is incorrect!', token: ''})
-                    }
-                })
-            }
-        })
-    })
+
+    }catch(err){
+        res.send({msg: `${err}`, token: ''})
+    }
 })
 
-app.listen(3000);
-console.log('server is running at 3000')
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
